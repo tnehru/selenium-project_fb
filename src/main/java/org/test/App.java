644b4e535cc6209cfe35998f221271a454a0dbd9
nehru_tangudu <nehru_tangudu@apple.com>
2024-10-12 01:37:59 -0700

    Selenium facebook project

---
+++ src/main/java/org/test/App.java
@@ -0,0 +1,50 @@
+package org.test;
+
+
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.By;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import java.io.File;
+import java.io.IOException;
+import java.time.Duration;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args ) throws InterruptedException, IOException {
+        System.out.println( "Hello World!" );
+
+        // WebDriver driver = new ChromeDriver();
+        ChromeOptions options = new ChromeOptions();
+        //options.addArguments("--headless");
+        WebDriver driver = new ChromeDriver(options);
+        //open URL
+        driver.get("https://facebook.com");
+        driver.manage().timeouts().implicitlyWait(Duration.ofMillis(500));
+        //locate contact button and click
+        Thread.sleep(2000);
+        driver.findElement(By.xpath("//*[@id=\"email\"]")).sendKeys("prem@pavan.com");
+        driver.findElement(By.xpath("//*[@id=\"pass\"]")).sendKeys("Pavan@123");
+        driver.findElement(By.xpath("/html/body/div[1]/div[1]/div[1]/div/div/div/div[2]/div/div[1]/form/div[2]/button")).click();
+        Thread.sleep(5000);
+
+        //capture screenshot
+        TakesScreenshot scrShot = ((TakesScreenshot) driver);
+        File srcFile = scrShot.getScreenshotAs(OutputType.FILE);
+        File destFile = new File("test-report.jpg");
+        FileUtils.copyFile(srcFile,destFile);
+
+        driver.quit();
+
+
+    }
+}
+
